54639fcc522d696b419619bc11b821be7950d679
john <jhezell@go.olemiss.edu>
2021-06-21 09:40:00 -0500

    Entries appear on dashboard.js

---
+++ components/EntryCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+    Button,
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    CardMedia,
+    MuiThemeProvider,
+    Typography
+} from "@material-ui/core";
+import Theme from "../styles/MuiTheme";
+
+export default function EntryCard({title, entry}) {
+    return (
+        <MuiThemeProvider theme={Theme}>
+            <Card className="w-11/12">
+                <h1>{title}</h1>
+                <h2>{entry}</h2>
+            </Card>
+        </MuiThemeProvider>
+    );
+}
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,27 +1,51 @@
 import {BottomNavigation, BottomNavigationAction} from "@material-ui/core";
 import {Book, AddBox, Timeline} from '@material-ui/icons';
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
+import fb from "../util/firebase-config";
+import SessionContext from "../util/SessionContext";
+import EntryCard from "../components/EntryCard";
+
+function useJournal() {
+    const {userProfile} = useContext(SessionContext)
+    const [journal, setJournal] = useState([])
+
+    useEffect(() => {
+        fb.firestore().collection(userProfile.uid).onSnapshot((snapshot => {
+            const newJournal = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data()
+            }))
+            setJournal(newJournal)
+        }))
+    }, [])
+    console.log(journal)
+    return journal
+}
 
 export default function Dashboard() {
+    const {userProfile} = useContext(SessionContext)
+    const journal = useJournal()
+
     return (
-
-            <div className="h-screen flex flex-col justify-center items-center bg-gray-50">
+            <div className="h-screen flex flex-col justify-center bg-gray-50">
                 {/*Body*/}
-                <div className="mb-auto h-10 bg-green-500">
-
+                <div className="flex flex-col space-y-4 items-center mb-auto my-4">
+                    {journal.map(journal =>
+                        <EntryCard
+                            title={journal.title}
+                            entry={journal.entry}
+                        />
+                    )}
                 </div>
 
                 {/*Footer*/}
                 <div className="w-screen h-25 bg-blue-500">
                     <BottomNavigation showLabels>
-                        <BottomNavigationAction label="Journal" icon={<Book/>} />
-                        <BottomNavigationAction label="Add" icon={<AddBox/>} />
-                        <BottomNavigationAction label="Timeline" icon={<Timeline/>} />
+                        <BottomNavigationAction label="Journal" icon={<Book/>}/>
+                        <BottomNavigationAction label="Add" icon={<AddBox/>}/>
+                        <BottomNavigationAction label="Timeline" icon={<Timeline/>}/>
                     </BottomNavigation>
                 </div>
             </div>
-
-
-
     )
 }
